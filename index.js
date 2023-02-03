06c57d25b03aeadae55d46b5f1f5a4774a68dc46
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2023-02-03 08:43:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require('fs');
 
 const main = async () => {
   try {
-    const textToSearch = core.getInput('textToSearch', { required: true });
-    const textToAdd = core.getInput('textToAdd', { required: true });
-    const paths = core.getInput('paths', {required: false});
+    const issueNumber = core.getInput('issueNumber', { required: true });
+    const assignees = core.getInput('assignee', { required: true });
 
-    const insertFileNameParameter = "{insertFileName}";
+    const token = core.getInput('GITHUB_TOKEN', { required: true })
+    const octokit = github.getOctokit(token)
+    
+    const { context } = github
 
-    if (paths !== null && paths.trim() !== "")
-    {
-        for (const path of paths.split(' ')) {
-
-            fs.readFile(path, (err, content) => {
-                if (err) throw err;
-    
-                if (!content.includes(textToSearch))
-                {
-                    var updatedTextToAdd = textToAdd;
-                    if (textToAdd.includes(insertFileNameParameter))
-                    {
-                        var encodedURI = encodeURIComponent(path);
-                        var encodedURIWIthoutExtension = encodedURI.substring(0, encodedURI.length - 3) // remove the .md at the end
-                        updatedTextToAdd = textToAdd.replace(insertFileNameParameter, encodedURIWIthoutExtension);
-                    }
-    
-                    var contentStr = content.toString();
-    
-                    contentStr = updatedTextToAdd + "\n\n" + contentStr;
-    
-                    fs.writeFile(path, contentStr, (err) => {
-    
-                    });
-                }
-            });
-        }
-    }
+    await octokit.rest.issues.addAssignees({
+      ...context.repo,
+      assignees,
+      issue_number: issueNumber,
+    })
   } catch (error) {
     core.setFailed(error.message);
   }
